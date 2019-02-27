21a86aca136bda82365b7bbb7c0d24b4c5b7a880
Dmitry Torba <dmitrytorba@gmail.com>
2019-02-27 13:41:16 -0800

    more recycler

--- app/src/main/java/com/example/d/geogeist/RecyclerController.java
+++ app/src/main/java/com/example/d/geogeist/RecyclerController.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -15,13 +17,44 @@ import java.util.List;
 
 class RecyclerController extends RecyclerView.Adapter<RecyclerController.ViewHolder> {
 
-    private List<String> dataset = new ArrayList<>();
+    private List<CensusViewItem> dataset = new ArrayList<>();
+
+    public static class CensusViewItem {
+        public String header;
+        public String subtitle;
+        public List<String> images = new ArrayList<>();
+
+        public CensusViewItem(JSONObject data) throws JSONException {
+            header = data.getString("name");
+            JSONObject population = data.getJSONObject("population");
+            JSONObject occupied = data.getJSONObject("occupied");
+            Integer totalPeople = population.getInt("total");
+            Integer houses = data.getInt("houses");
+            subtitle = withSuffix(totalPeople) + " people in " + withSuffix(houses) + " houses";
+            String map = data.optString("map");
+            if (map != null) {
+                images.add(MainActivity.IMG_URL + map);
+            }
+            images.add(MainActivity.IMG_URL + population.getString("chart"));
+            images.add(MainActivity.IMG_URL + occupied.getString("race_chart"));
+            images.add(MainActivity.IMG_URL + occupied.getString("household_chart"));
+            images.add(MainActivity.IMG_URL + occupied.getString("finance_chart"));
+        }
+    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView textView;
+        public TextView header;
+        public TextView subtitle;
+        public ListView imagesView;
+        public ArrayAdapter<String> imagesAdapter;
+
         public ViewHolder(View itemView) {
             super(itemView);
-            textView = itemView.findViewById(R.id.text_view);
+            header = itemView.findViewById(R.id.title);
+            subtitle = itemView.findViewById(R.id.subtitle);
+            imagesView = itemView.findViewById(R.id.image_list);
+            imagesAdapter = new ArrayAdapter<>(imagesView.getContext(), android.R.layout.simple_list_item_1);
+            imagesView.setAdapter(imagesAdapter);
         }
     }
 
@@ -41,7 +74,12 @@ class RecyclerController extends RecyclerView.Adapter<RecyclerController.ViewHol
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.textView.setText(dataset.get(position));
+        CensusViewItem item = dataset.get(position);
+        holder.header.setText(item.header);
+        holder.subtitle.setText(item.subtitle);
+        holder.imagesAdapter.clear();
+        holder.imagesAdapter.addAll(item.images);
+        holder.imagesAdapter.notifyDataSetChanged();
     }
 
     @Override
@@ -52,32 +90,42 @@ class RecyclerController extends RecyclerView.Adapter<RecyclerController.ViewHol
     public void loadData(JSONObject data) {
         try {
             dataset.clear();
+
             JSONObject tract = data.optJSONObject("tract");
             if (tract != null) {
-                String tractName = tract.getString("name");
-                dataset.add(tractName);
+                CensusViewItem item = new CensusViewItem(tract);
+                dataset.add(item);
             }
 
             JSONObject place = data.optJSONObject("place");
             if (place != null) {
-                String placeName = place.getString("name");
-                dataset.add(placeName);
+                CensusViewItem item = new CensusViewItem(place);
+                dataset.add(item);
             }
 
             JSONObject county = data.optJSONObject("county");
             if (county != null) {
-                String countyName = county.getString("name");
-                dataset.add(countyName);
+                CensusViewItem item = new CensusViewItem(county);
+                dataset.add(item);
             }
 
             JSONObject state = data.optJSONObject("state");
             if (state != null) {
-                String stateName = state.getString("name");
-                dataset.add(stateName);
+                CensusViewItem item = new CensusViewItem(state);
+                dataset.add(item);
             }
+
             this.notifyDataSetChanged();
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    public static String withSuffix(long count) {
+        if (count < 1000) return "" + count;
+        int exp = (int) (Math.log(count) / Math.log(1000));
+        return String.format("%.1f%c",
+                count / Math.pow(1000, exp),
+                "KMGTPE".charAt(exp-1));
     }
 }
